5b5d81cfeb4afc20412732b4730b1884f594677d
Krzysztof Klimek <klimek.krzy@gmail.com>
2019-01-25 01:09:44 +0100

    last commit

--- build/generated-sources/ap-source-output/entities/Grupa_.java
+++ build/generated-sources/ap-source-output/entities/Grupa_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-01-24T02:21:11")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-01-24T23:52:02")
 @StaticMetamodel(Grupa.class)
 public class Grupa_ { 
 
--- build/generated-sources/ap-source-output/entities/Klient_.java
+++ build/generated-sources/ap-source-output/entities/Klient_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-01-24T02:21:11")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-01-24T23:52:02")
 @StaticMetamodel(Klient.class)
 public class Klient_ { 
 
